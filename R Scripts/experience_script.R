7026ce87cf3ec02502d75ee237c317e946d68006
Toph3 <mbc225@cornell.edu>
2021-02-10 15:44:07 -0500

    Update experience_script.R

--- R Scripts/experience_script.R
+++ R Scripts/experience_script.R
@@ -10,7 +10,18 @@ myfile <- "~/Documents/Most/-SLA_Study/GitHub/ActionLearning/Data/SLA Official D
 df2 <- read_csv(myfile)
 View(df2)
 str(df2)
+df2<-df2[!(df2$Finished=="0"),]
 
 #MC: histogram of total experience scores.
 ggplot(df2, aes(x = `Experience_Total`, na.rm = TRUE)) +
   geom_histogram(color = "black", fill = "white", stat = "bin", binwidth = 5)
+
+
+#MC: regressions for comprehension and production as predicted by experience.
+#MC: still need to combine data frames to be absolutely sure of what's going on here. Check out cbind.
+model_experience_comprehension <- lm(df$Experience_Total ~ df2$Experience_Total)
+summary(model_experience_comprehension)
+
+
+model_experience_production <- lm(df$Correct_Mvt_Scores ~ df2$Experience_Total)
+summary(model_experience_production)
