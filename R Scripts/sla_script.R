a3ad2af3b5f270dc1443256d7447aa77517ff48f
Toph3 <mbc225@cornell.edu>
2021-03-04 20:46:06 -0500

    Create sla_script.R

---
+++ R Scripts/sla_script.R
@@ -0,0 +1,13 @@
+# MC: I'm creating this as a slightly more abbreviated file than statistical_action_learning_script.R.
+# I hope to consolidate the most relevant code here, and will probably be doing cleaning and editing
+# in this file from now on. (4 March 2021)
+
+### GETTING STARTED ###
+library(readr)   #reading CSV's
+library(kableExtra)   #formatting table
+library(pastecs) #describes data
+library(dplyr)   #manipulating data to view it
+library(ggplot2) #visualizing data with graphs (we don't talk about the first "ggplot")
+library(emmeans) #post hoc analyses (contrasts!)
+library(magrittr) #not sure if I need this one?
+
